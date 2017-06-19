65c7929e9f1cb570a78232b06c945b3a5624b9c0
Andre Pratama <Andre Pratama>
2017-06-19 10:09:53 +0700

    set env

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', getenv('MYSQL_USER'));
 define('DB_PASSWORD', getenv('MYSQL_PASSWORD'));
 
 /** MySQL hostname */
-define('DB_HOST', getenv('MYSQL_SERVICE_HOST'));
+define('DB_HOST', getenv('WORDPRESSDB_SERVICE_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
